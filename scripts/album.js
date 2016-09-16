569efd165754a51f726fd3bfe1c8aff3b1a398e2
Justin L. Lattimore <jlattimore3d@gmail.com>
2016-09-16 03:40:03 -0400

    completed assignment-11-domScriptingAlbumView

--- scripts/album.js
+++ scripts/album.js
@@ -28,6 +28,21 @@ var albumMarconi = {
     ]
 };
 
+var albumNotorious = {
+    title: 'Born Again',
+    artist: 'Notorious B.I.G.',
+    label: 'EM',
+    year: '1980',
+    albumArtUrl: 'https://pbs.twimg.com/profile_images/1968025269/NotoriousBIG.jpg',
+    songs: [
+        { title: 'Big Pappa', duration: '1:01' },
+        { title: 'Hypnotize', duration: '5:01' },
+        { title: 'Juicy', duration: '3:21'},
+        { title: 'Old thing back', duration: '3:14' },
+        { title: 'Mo money mo problems', duration: '2:15'}
+    ]
+};
+
 var createSongRow = function(songNumber, songName, songLength) {
      var template =
         '<tr class="album-view-song-item">'
@@ -40,14 +55,16 @@ var createSongRow = function(songNumber, songName, songLength) {
      return template;
  };
 
+var albumTitle = document.getElementsByClassName('album-view-title')[0];
+var albumArtist = document.getElementsByClassName('album-view-artist')[0];
+var albumReleaseInfo = document.getElementsByClassName('album-view-release-info')[0];
+var albumImage = document.getElementsByClassName('album-cover-art')[0];
+var albumSongList = document.getElementsByClassName('album-view-song-list')[0];
+ 
+
  var setCurrentAlbum = function(album) {
      // #1
-     var albumTitle = document.getElementsByClassName('album-view-title')[0];
-     var albumArtist = document.getElementsByClassName('album-view-artist')[0];
-     var albumReleaseInfo = document.getElementsByClassName('album-view-release-info')[0];
-     var albumImage = document.getElementsByClassName('album-cover-art')[0];
-     var albumSongList = document.getElementsByClassName('album-view-song-list')[0];
- 
+     
      albumTitle.firstChild.nodeValue = album.title;
      albumArtist.firstChild.nodeValue = album.artist;
      albumReleaseInfo.firstChild.nodeValue = album.year + ' ' + album.label;
@@ -62,4 +79,14 @@ var createSongRow = function(songNumber, songName, songLength) {
  
  window.onload = function() {
      setCurrentAlbum(albumPicasso);
+     
+     var albums = [albumPicasso, albumMarconi, albumNotorious];
+     var index = 1;
+     albumImage.addEventListener("click", function(event){
+         setCurrentAlbum(albums[index]);
+         index++;
+         if(index == albums.length){
+             index = 0;
+         }
+     });
  };
